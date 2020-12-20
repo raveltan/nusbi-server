ba2518189c1a3bd38e6bcc60f87d7b5f3f3cf590
Ravel Tanjaya <raveltanjaya@outlook.com>
2020-12-20 14:40:12 +0700

    added gpa

--- student/getProfile.go
+++ student/getProfile.go
@@ -14,7 +14,7 @@ type getProfileResponse struct{
 	Gender string
 	DOB string
 	Email string
-	GPA sql.NullInt32
+	GPA sql.NullFloat64
 	SCU sql.NullInt32
 	batch int
 	Major string
@@ -29,12 +29,14 @@ func GetStudentProfile(c *fiber.Ctx) error {
 		return c.SendStatus(403)
 	}
 	sqlResult, err := db.Db.Query(
-		"select first_name,last_name,gender,dob,email,gpa,scu,batch,m.major_name from Students s,Majors m where user_id = ? and m.major_id = s.major_id",id)
+		"select first_name,last_name,gender,dob,email, (select sum(scu) from Courses c,Enrolled_Courses e, Class cl where e.student_id = (select s.student_id from Students s where s.user_id = ?) and e.class_id = cl.class_id and cl.course_id = c.course_id) as scu ,batch,m.major_name from Students s,Majors m where user_id = ? and m.major_id = s.major_id",id,id)
 	if err != nil {
+		log.Println("-1")
 		log.Println(err)
 		return c.SendStatus(500)
 	}
 	if sqlResult.Err() != nil {
+		log.Println("0")
 		log.Println(err)
 		return c.SendStatus(500)
 	}
@@ -42,14 +44,59 @@ func GetStudentProfile(c *fiber.Ctx) error {
 	f := false
 	for sqlResult.Next() {
 		f = true
-		err = sqlResult.Scan(&r.FirstName,&r.LastName,&r.Gender,&r.DOB,&r.Email,&r.GPA,&r.SCU,&r.batch,&r.Major)
+		err = sqlResult.Scan(&r.FirstName,&r.LastName,&r.Gender,&r.DOB,&r.Email,&r.SCU,&r.batch,&r.Major)
 		if err != nil {
 			log.Println(err)
+			log.Println("6")
 			return c.SendStatus(500)
 		}
 	}
 	if !f {
 		return c.SendStatus(404)
+	}
+	res2, err := db.Db.Query(
+		"select e.mid_score,e.final_score from Enrolled_Courses e where e.student_id = (select student_id from Students s where s.user_id = ?)",id)
+	if err != nil {
+		log.Println(err)
+		return c.SendStatus(500)
+	}
+
+	scores := []float64{}
+	count := 0
+	v := false
+	for res2.Next() {
+		v = true
+		var mid int
+		var final int
+		err = res2.Scan(&mid,&final)
+		if err != nil {
+			return c.SendStatus(500)
+		}
+		if mid < 0 || final < 0{
+			v = false
+			break
+		}
+		count++
+		scores = append(scores, ((float64(mid)*0.3) + (float64(final)*0.7))*0.04)
+	}
+	if res2.Err() != nil {
+		log.Println(err)
+		return c.SendStatus(500)
+	}
+	if !v {
+		r.GPA = sql.NullFloat64{
+			Float64: 0.01,
+			Valid:   false,
+		}
+		return c.JSON(r)
+	}
+	gpa := 0.0
+	for _,score:=range scores{
+		gpa += score
+	}
+	r.GPA = sql.NullFloat64{
+		Float64: gpa/float64(count),
+		Valid:   true,
 	}
 	return c.JSON(r)
 }
